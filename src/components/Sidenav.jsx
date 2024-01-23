f52b9f53ef4daac64f5993d22a610c7f6a2adffd
Opeoluwa-Ogunlaja <ogunlajafaith@gmail.com>
2024-01-23 18:00:05 +0100

    some sidenav adjustments

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,7 +1,70 @@
 import React from 'react'
 
+const sidenav_links = [
+    {
+        to: '/',
+        icon: '',
+        title: 'home'
+    },
+    {
+        to: '/',
+        icon: '',
+        title: 'home2'
+    },
+    {
+        to: '/',
+        icon: '',
+        title: 'hom3'
+    },
+    {
+        to: '/',
+        icon: '',
+        title: 'home4'
+    },
+    {
+        to: '/',
+        icon: '',
+        title: 'home5'
+    },
+    {
+        to: '/',
+        icon: '',
+        title: 'home6'
+    },
+    {
+        to: '/',
+        icon: '',
+        title: 'home7'
+    },
+    {
+        to: '/',
+        icon: '',
+        title: 'home8'
+    }
+]
+
 export const Sidenav = () => {
   return (
-    <aside className='sidenav'>sidenav</aside>
+    <aside className='sidenav'>
+        <div className='logo'></div>
+        <ul>
+            {sidenav_links.slice(0, -3).map((link, i) => {
+                return <li key={link.title}>
+                    <a href={link.to}>
+                        <span className='visually-hidden'>{link.title}</span>
+                    </a>
+                </li>
+            })}
+            <div className='sidenav-bottom'>
+                {sidenav_links.slice(-3, -1).map((link, i) => {
+                    return <li key={link.title}>
+                        <a href={link.to}>
+                            <span className='visually-hidden'>{link.title}</span>
+                        </a>
+                    </li>
+                })}
+            </div>
+        </ul>
+    </aside>
   )
 }
