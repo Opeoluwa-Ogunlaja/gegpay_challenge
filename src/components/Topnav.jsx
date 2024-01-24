5c1cc76871307889bd916bca6e984110f6764696
Opeoluwa-Ogunlaja <ogunlajafaith@gmail.com>
2024-01-24 23:52:41 +0100

    some style cleanup and preparations for navbar responsiveness rework

--- src/components/Topnav.jsx
+++ src/components/Topnav.jsx
@@ -11,33 +11,33 @@ const Icon = ({ IconElement }) => {
 }
 export const Topnav = () => {
   return (
-    <header className='topnav'>
+    <header className='topnav flex items-center justify-between gap-md'>
         <div className='topnav_left'>
           <p className='leading-heading-1'>Dashboard</p>
         </div>
 
-        <div className='topnav_right'>
+        <div className='topnav_right flex items-center justify-center gap-md'>
           <div className="search_bar">
             <div>
               <img src={Search} alt="" />
               <input type="text" placeholder='Search...' />
             </div>
           </div>
-          <div className="calendar_and_notification">
-            <div className="topnav_calender">
+          <div className="calendar_and_notification flex items-center justify-between gap-md">
+            <div className="topnav_calender flex items-center cursor">
               <img src={Calender} alt="Calender_icon" />
               <p>January 23, 2024</p>
             </div>
 
-            <div className="topnav_notification">
+            <div className="topnav_notification pointer flex items-center justify-center">
               <img src={Notification} alt="Calender_icon" />
 
             </div>
           </div>
           <div className="profile_menu">
-            <div className="profile_container">
+            <div className="profile_container flex items-center justify-center cursor">
               <img src={Profile} alt="User Profile" />
-              <div className="name_and_email">
+              <div className="name_and_email flex flex-column">
                 <b>Justin Bergson</b>
                 <p>Justin@gmail.com</p>
               </div>
