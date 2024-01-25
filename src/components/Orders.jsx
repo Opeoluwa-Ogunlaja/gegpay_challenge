aeb59e36566f0b1f5fda393958cd72ccf059cb31
Opeoluwa-Ogunlaja <ogunlajafaith@gmail.com>
2024-01-25 01:29:33 +0100

    professional looking chart implemented

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export const Orders = () => {
   return (
-    <section className='orders'>Orders</section>
+    <section className='orders'>
+      <h2 className='section-header'>Last Orders</h2>
+    </section>
   )
 }
--- src/components/TrendsGraph.jsx
+++ src/components/TrendsGraph.jsx
@@ -1,7 +1,73 @@
-import React from 'react'
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement
+} from 'chart.js'
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement
+)
+
+import { Chart } from "react-chartjs-2";
+
+const data = {
+  labels: ['Jan', 'Feb', "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+  datasets: [{
+    barPercentage: 0.5,
+    barThickness: 30,
+    data: [65, 59, 80, 81, 56, 55, 40, 65, 59, 80, 81, 56],
+    backgroundColor: 'hsla(165, 59%, 50%, .1)',
+    borderWidth: 0,
+    borderRadius: 9999,
+    hoverBackgroundColor: 'hsla(165, 59%, 50%, 1)'
+  }]
+};
+
+const options = {
+  scales: {
+    y: {
+      beginAtZero: true
+    }
+  },
+  plugins: {
+    tooltip: {
+      yAlign: 'bottom',
+      callbacks : {
+        label: () => ''
+      }
+    },
+    legend: {
+      display: false
+    }
+  }
+};
+
 
 export const TrendsGraph = () => {
   return (
-    <section className='trends'>TrendsGraph</section>
-  )
-}
+    <section className="trends">
+      <div className="flex items-center justify-between">
+        <h2 className="section-header">Sales Trends</h2>
+        <div className="flex items-center">
+          <h4>Sort by: </h4>
+          <button>Weekly</button>
+        </div>
+      </div>
+      <Chart options={options} data={data} type={'bar'}/>
+    </section>
+  );
+};
