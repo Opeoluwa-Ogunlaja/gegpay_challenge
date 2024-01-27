5096969076d1761c088b9f996e651f70425a0c97
Opeoluwa-Ogunlaja <ogunlajafaith@gmail.com>
2024-01-27 20:08:43 +0100

    done

--- src/components/Topnav.jsx
+++ src/components/Topnav.jsx
@@ -39,13 +39,13 @@ export const Topnav = () => {
         </div>
         <div className="profile_menu">
           <div className="profile_container flex items-center justify-center cursor">
-            <Icon iconelement={ProfileIcon} alt="User Profile" />
+            <Icon iconelement={ProfileIcon} style={{ borderRadius: 'inherit' }} alt="User Profile" />
             <div className="name_and_email flex flex-column hide-on-small">
               <b className="clr-neutral-900">Justin Bergson</b>
               <p>Justin@gmail.com</p>
             </div>
             <div className="caret-down">
-              <Icon iconelement={ArrowDownIcon} alt="" />
+              <Icon iconelement={ArrowDownIcon} className="aspect-square" alt="" />
             </div>
           </div>
         </div>
