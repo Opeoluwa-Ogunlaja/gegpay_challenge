b0d3176b3bffb1f644b0c632ff4b26924fd9e3aa
john-shaibu <johnmaxwell059@gmail.com>
2024-01-28 00:43:09 +0100

    calendar functionality implemented

--- src/components/Topnav.jsx
+++ src/components/Topnav.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 
 import { Icon } from "./Icon";
 import {
@@ -7,15 +8,18 @@ import {
   SearchIcon,
 } from "../assets/icons";
 import { ProfileDropdown } from "./ProfileDropdown";
+import Calendar from 'react-calendar'
 
 export const Topnav = () => {
+  const [dateValue, setDate] = useState(new Date());
+  // const today = new Date();
+  // console.log(today)
+  const onChange = (newDate) => {
+    setDate(newDate);
+  };
   return (
     <header className="topnav flex items-center justify-between gap-md">
       <div className="topnav_left flex justify-center items-center gap-md ">
-        {/* <div className="logo logo-on-small">
-          <img src="/icons/logo.svg" alt="logo" />
-          <h1 className="visually-hidden">Site Logo</h1>
-        </div> */}
         <p className="leading-wider clr-neutral-900">Dashboard</p>
       </div>
 
@@ -29,7 +33,10 @@ export const Topnav = () => {
         <div className="calendar_and_notification flex items-center justify-between">
           <div className="topnav_calender flex items-center cursor">
             <Icon iconelement={CalenderIcon} alt="Calender_icon" />
-            <p className="hide-on-small clr-neutral-900">January 23, 2024</p>
+            <p className="hide-on-small clr-neutral-900">{}</p>
+            <div className="calendar-dropdown">
+              <Calendar onChange={onchange} />
+            </div>
           </div>
 
           <div className="topnav_notification pointer flex items-center justify-center">
