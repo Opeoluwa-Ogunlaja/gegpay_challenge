3030158fbabdeadd1368d72727a59ab50ac79a0e
Opeoluwa-Ogunlaja <ogunlajafaith@gmail.com>
2024-01-28 01:30:19 +0100

    calendar 40%

---
+++ src/components/CalendarDropdown.jsx
@@ -0,0 +1,48 @@
+import React, { useRef, useState } from "react";
+import { CalenderIcon } from "../assets/icons";
+import Calendar from "react-calendar";
+import { Icon } from "./Icon";
+import { useToggle } from "../hooks/useToggle";
+import useClickOutside from "../hooks/useClickOutside";
+
+const  months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ]
+
+export const CalendarDropdown = () => {
+  const [dropdownOpen, toggleDropdownOpen, set] = useToggle(false);
+  const dropdownRef = useRef(null);
+
+  useClickOutside(dropdownRef, (e) => {
+    set(false);
+  });
+
+  const [dateValue, setDate] = useState(new Date());
+  const onChange = (newDate) => {
+    setDate(newDate);
+  };
+  return (
+    <div className="calendar-dropdown" ref={dropdownRef}>
+      <div className="topnav_calender flex items-center cursor" onClick={toggleDropdownOpen}>
+        <Icon iconelement={CalenderIcon} alt="Calender_icon" />
+        <p className="text-md font-medium">{months[dateValue.getMonth()]} {dateValue.getDate()}, {' '} {dateValue.getFullYear()} </p>
+      </div>
+      <div
+        className={`calendar-container-content${dropdownOpen ? " open" : ""}`}
+      >
+        <Calendar onChange={onChange} />
+      </div>
+    </div>
+  );
+};
--- src/components/Topnav.jsx
+++ src/components/Topnav.jsx
@@ -1,22 +1,11 @@
 import React from "react";
-import { useState } from "react";
 
 import { Icon } from "./Icon";
-import {
-  CalenderIcon,
-  NotificationIcon,
-  SearchIcon,
-} from "../assets/icons";
+import { NotificationIcon, SearchIcon } from "../assets/icons";
 import { ProfileDropdown } from "./ProfileDropdown";
-import Calendar from 'react-calendar'
+import { CalendarDropdown } from "./CalendarDropdown";
 
 export const Topnav = () => {
-  const [dateValue, setDate] = useState(new Date());
-  // const today = new Date();
-  // console.log(today)
-  const onChange = (newDate) => {
-    setDate(newDate);
-  };
   return (
     <header className="topnav flex items-center justify-between gap-md">
       <div className="topnav_left flex justify-center items-center gap-md ">
@@ -31,14 +20,7 @@ export const Topnav = () => {
           </div>
         </div>
         <div className="calendar_and_notification flex items-center justify-between">
-          <div className="topnav_calender flex items-center cursor">
-            <Icon iconelement={CalenderIcon} alt="Calender_icon" />
-            <p className="hide-on-small clr-neutral-900">{}</p>
-            <div className="calendar-dropdown">
-              <Calendar onChange={onchange} />
-            </div>
-          </div>
-
+          <CalendarDropdown />
           <div className="topnav_notification pointer flex items-center justify-center">
             <Icon iconelement={NotificationIcon} alt="Notification_icon" />
           </div>
